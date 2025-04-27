89e3326fa09cd69c072d750320699b8598aa8a14
Abhishekshinde12 <shindeahbishek36912@gmail.com>
2025-04-27 13:26:46 +0530

    Added the code to analyze the mp4 video. The mp4 file is sent to fastapi on github and predictions are returned. So now the complete real time module has got completed

--- backend/audio_video/video_model.py
+++ backend/audio_video/video_model.py
@@ -1,8 +1,17 @@
-import time 
-
-def video_features_input(path):
-    pass
+import requests
 
 def predict_video_emotion(path):
-    time.sleep(1)
-    return "neutral"
+    try:
+        url = "https://urban-space-guide-jv4jrjjqq79cw65-8000.app.github.dev/predict"
+        with open(path, "rb") as f:
+            files = {"file": ("chunk.mp4", f, "video/mp4")}
+            resp = requests.post(url, files=files)
+            resp.raise_for_status()
+            result = resp.json()['dominant_emotion']
+            print(type(result))
+            # print("Predicted emotion:", result)
+            print("Response : ", resp)
+            print("Result :", result)
+            return result
+    except Exception as e:
+        print(e)
